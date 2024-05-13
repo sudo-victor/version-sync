be7a2ac78c569091775e7eed9a62ddcb2c3ecd55
Victor de Souto <victor.souto@hotmail.com>
2024-05-13 12:03:09 -0300

    feat: adjust to bin

--- src/index.mjs
+++ src/index.mjs
@@ -18,14 +18,30 @@ function execCommand(command) {
   }
 }
 
+function fetchTags() {
+  console.log('Buscando tags do repositório remoto...');
+  execCommand('git fetch --tags');
+}
+
 function getLastReleaseTag() {
-  return execCommand('git describe --tags --abbrev=0');
+  try {
+    fetchTags();
+    return execCommand('git describe --tags --abbrev=0');
+  } catch (error) {
+    console.log('Nenhuma tag encontrada. Usando o primeiro commit como referência.');
+    return execCommand('git rev-list --max-parents=0 HEAD');
+  }
 }
 
 function getGitDiff() {
   const lastTag = getLastReleaseTag();
-  const diff = execCommand(`git diff ${lastTag} HEAD`);
-  return diff;
+  if (lastTag) {
+    const diff = execCommand(`git diff ${lastTag} HEAD`);
+    return diff;
+  } else {
+    console.log('Nenhum commit anterior encontrado para comparar.');
+    return '';
+  }
 }
 
 function incrementVersion(version, changeType) {
